018633c3ca70db89ae87540e7d40c1e21180a783
Jeniffer Fretes França <jeniffer.franca@brasil317.com.br>
2024-02-02 12:55:11 -0300

    Iniciando o processo

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using OpenQA.Selenium.Chrome;
+
+public static class HelloSelenium
+{
+    public static void Main()
+    {
+        var driver = new ChromeDriver();
+            
+        driver.Navigate().GoToUrl("https://store.steampowered.com/search/?filter=topsellers");
+            
+        //driver.Quit();
+    }
+}
