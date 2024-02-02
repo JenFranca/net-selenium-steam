559d83a2e477af860c54b4d9f932cd40ef020885
Jeniffer Fretes França <jeniffer.franca@brasil317.com.br>
2024-02-02 15:14:22 -0300

    adicionando id

--- Program.cs
+++ Program.cs
@@ -1,13 +1,20 @@
-﻿using OpenQA.Selenium.Chrome;
+﻿using System.Collections.ObjectModel;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
 
 public static class HelloSelenium
 {
     public static void Main()
     {
         var driver = new ChromeDriver();
-            
+
         driver.Navigate().GoToUrl("https://store.steampowered.com/search/?filter=topsellers");
-            
-        //driver.Quit();
+
+        IWebElement tableElement = driver
+        .FindElement(By.Id("search_results"));
+
+        Console.WriteLine(tableElement);
+
+        driver.Quit();
     }
 }
