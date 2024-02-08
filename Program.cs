b5d37b1793beb93bdb9b3837009b63402c9fc5b5
Jeniffer Fretes França <jeniffer.franca@brasil317.com.br>
2024-02-08 14:30:48 -0300

    quase lá

--- Program.cs
+++ Program.cs
@@ -10,10 +10,54 @@ public static class HelloSelenium
 
         driver.Navigate().GoToUrl("https://store.steampowered.com/search/?filter=topsellers");
 
-        IWebElement tableElement = driver
-        .FindElement(By.Id("search_results"));
+        ReadOnlyCollection<IWebElement> tableElement = driver
+        .FindElement(By.Id("search_resultsRows"))
+        .FindElements(By.ClassName("responsive_search_name_combined"));
 
-        Console.WriteLine(tableElement);
+        foreach (var tableRow in tableElement)
+        {
+            try
+            {
+                var name = tableRow
+                    .FindElement(By.ClassName("title")).Text;
+                Console.WriteLine($"Nome do jogo: {name}");
+                
+                var date = tableRow
+                    .FindElement(By.ClassName("search_released")).Text;
+                Console.WriteLine($"Data de lançamento: {date}");
+                
+                var rating = tableRow
+                    .FindElement(By.ClassName("search_review_summary"))
+                    .GetDomAttribute("class");
+                Console.WriteLine($"Avaliação: {rating}");
+
+                // if (rating != null) 
+                // {
+                //     rating = "search_review_summary positive"; 
+                //     Console.WriteLine("Positivo");
+                // }
+                // else
+                // {
+                //     rating = "search_review_summary mixed"; 
+                //     Console.WriteLine("neutro");
+                // }
+
+                var price = tableRow
+                    .FindElement(By.ClassName("discount_original_price")).Text;
+                Console.WriteLine($"Valor do jogo: {price}");
+
+                var discount = tableRow
+                    .FindElement(By.ClassName("discount_final_price")).Text;
+                Console.WriteLine($"Valor do jogo com desconto: {discount}");
+            }
+            catch (System.Exception)
+            {
+
+            }
+
+        }
+
+        // Console.WriteLine(tableElement.First().Text);
 
         driver.Quit();
     }
